bacbba3d22810bd3d090c57502c3646859e6eb8c
Shturmeman <valerasasin@gmail.com>
2019-08-30 14:18:52 +0300

    six

--- chank/project.js
+++ chank/project.js
@@ -41,7 +41,8 @@ class ProjectCard extends HTMLElement {
                 top: 50%;
                 left:50%;
                 transform:translate(-50%);
-                color:red;
+                color:white;
+                background : #00000085;
                 font-size:18px;
                 z-index:1;
                 text-align:center;
